5cca98ed90ab22ae12895eaf0f6c5f75011559fe
bl4ko <gasperoblak@bl4ko.com>
2024-05-09 00:43:47 +0200

    test: fixing http_test

--- internal/utils/http_test.go
+++ internal/utils/http_test.go
@@ -1,46 +1,39 @@
 package utils
 
 import (
-	"crypto/tls"
 	"net/http"
-	"reflect"
 	"testing"
 )
 
 func TestNewHTTPClient(t *testing.T) {
-	wantInsecureClient := &http.Client{Transport: &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}}
-	gotInsecureClient, err := NewHTTPClient(false, "")
+	_, err := NewHTTPClient(false, "")
 	if err != nil {
 		t.Errorf("not expecting error, but got: %s", err)
-	}
-	if !reflect.DeepEqual(wantInsecureClient, gotInsecureClient) {
-		t.Errorf("want: %v, got: %v", wantInsecureClient, gotInsecureClient)
 	}
 
 	// wrong path
 	_, err = NewHTTPClient(true, "\\//")
 	if err == nil {
-		t.Error("want error but got none")
+		t.Error("expected error but got none")
 	}
 
-	customCertPool, _ := LoadExtraCert("")
-	wantHTTPClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: customCertPool,
-			},
-		},
-	}
-	gotHTTPClient, err := NewHTTPClient(true, "")
+	// Check if `InsecureSkipVerify` is set correctly
+	insecureClient, err := NewHTTPClient(false, "")
 	if err != nil {
-		t.Errorf("don't want error but got: %s", err)
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	transport := insecureClient.Transport.(*http.Transport) //nolint:forcetypeassert
+	if transport.TLSClientConfig.InsecureSkipVerify != true {
+		t.Errorf("expected InsecureSkipVerify to be true, got false")
 	}
 
-	if !reflect.DeepEqual(wantHTTPClient, gotHTTPClient) {
-		t.Errorf("want: %v, got %v", wantHTTPClient, gotHTTPClient)
+	// Check if RootCAs is set when expected
+	certClient, err := NewHTTPClient(true, "")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	transport = certClient.Transport.(*http.Transport) //nolint:forcetypeassert
+	if transport.TLSClientConfig.RootCAs == nil {
+		t.Errorf("expected RootCAs to be set, got nil")
 	}
 }
